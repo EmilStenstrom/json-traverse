290eb47493b91ca7e27975e860fb9d07c16461f0
Emil Stenström <em@kth.se>
2016-12-04 21:06:14 +0100

    Initial commit.

---
+++ jsontraverse/parser.py
@@ -0,0 +1,78 @@
+from __future__ import unicode_literals
+import re
+import json
+
+class JsonMultiParser:
+    def __init__(self, raw_data):
+        if raw_data and not isinstance(raw_data, "".__class__):
+            raise TypeError("the 'raw_data' argument must be {}, not '{}'".format("".__class__, type(raw_data)))
+
+        self.data = json.loads(raw_data) if raw_data else None
+
+    def traverse(self, path):
+        if not isinstance(path, "".__class__):
+            raise TypeError("the 'path' argument must be {}, not '{}'".format("".__class__, type(path)))
+
+        reduced = []
+
+        if self.data:
+            reduced.append(self.data)
+
+        if path:
+            for item in path.split("."):
+                list_reduced = list(reduced)
+                if self.is_valid_index(item):
+                    list_reduced = self.reduce_list(reduced, item)
+
+                dict_reduced = self.flatten(reduced)
+                if list_reduced == reduced:
+                    dict_reduced = self.reduce_dict(dict_reduced, item)
+
+                if list_reduced != reduced:
+                    reduced = list_reduced
+                elif dict_reduced != self.flatten(reduced):
+                    reduced = dict_reduced
+                else:
+                    break
+
+        if isinstance(reduced, list) and len(reduced) == 1:
+            reduced = reduced[0]
+
+        if isinstance(reduced, list) and len(reduced) == 0:
+            reduced = ''
+
+        return reduced
+
+    def reduce_list(self, reduced, item):
+        outputs = []
+
+        for value in reduced:
+            try:
+                outputs.append(value[int(item)])
+            except (ValueError, IndexError, KeyError, TypeError):
+                outputs.append(value)
+
+        return outputs
+
+    def reduce_dict(self, reduced, item):
+        outputs = []
+
+        for value in reduced:
+            try:
+                outputs.append(value[item])
+            except (KeyError, TypeError):
+                outputs.append(value)
+
+        return outputs
+
+    def flatten(self, reduced):
+        flattened = []
+        for value_or_sublist in reduced:
+            if isinstance(value_or_sublist, list):
+                flattened += value_or_sublist
+            else:
+                flattened.append(value_or_sublist)
+        return flattened
+
+    def is_valid_index(self, string):
+        return re.match(r"^(0|[1-9][0-9]*)$", string)
