a02322cfaeb35ca99f8bcf1d784c606f2b06a1c2
Emil Stenström <em@kth.se>
2016-12-04 21:07:39 +0100

    Add packaging information.

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+VERSION = '0.1'
+PKG_NAME = 'json-traverse'
+
+setup(
+    name=PKG_NAME,
+    packages=[PKG_NAME.replace("-", "")],
+    version=VERSION,
+    description='Query complex JSON structures using a simple query syntax.',
+    author=u'Emil Stenström',
+    author_email='em@kth.se',
+    url='https://github.com/EmilStenstrom/{}/'.format(PKG_NAME),
+    download_url='https://github.com/EmilStenstrom/{}/archive/{}.zip'.format(PKG_NAME, VERSION),
+    install_requires=[],
+    keywords=['json', 'query', 'tree', 'parser', 'json-query'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+)
